2cdf5042de4f1c04e4fb334427d9d4eba525b4b4
einar tomter <einatomter@gmail.com>
2023-04-12 19:42:14 +0200

    fix CMake warning, add clahe to compressed imgs

--- vocab/publish_clahe.py
+++ vocab/publish_clahe.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 
-class ImageSubscriber:
+image_topic = "/camera/image_raw"
+image_topic2 = "/slave1/image_raw/compressed"
+clahe_clip_limit = 10.0
+clahe_grid_size = (6,6)
 
+class ImageSubscriber:
     def __init__(self):
+
         self.bridge = CvBridge()
-        self.subscriber = rospy.Subscriber("/camera/image_raw", Image, self.callback)
+
+        self.subscriber = rospy.Subscriber(image_topic, Image, self.callback)
+        self.subscriber_compressed = rospy.Subscriber(image_topic2, CompressedImage, self.callback_compressed)
+
         self.publisher = rospy.Publisher("/image_clahe", Image, queue_size=1)
 
     def callback(self, data):
@@ -20,7 +28,24 @@ class ImageSubscriber:
             rospy.logerr(e)
 
         # Apply CLAHE on the image
-        clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(6,6))
+        clahe = cv2.createCLAHE(clipLimit=clahe_clip_limit, tileGridSize=clahe_grid_size)
+        clahe_image = clahe.apply(cv_image)
+
+        try:
+            self.publisher.publish(self.bridge.cv2_to_imgmsg(clahe_image, "mono8"))
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+
+    def callback_compressed(self, data):
+        np_arr = np.frombuffer(data.data, np.uint8)
+        try:
+            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+        # Apply CLAHE on the image
+        clahe = cv2.createCLAHE(clipLimit=clahe_clip_limit, tileGridSize=clahe_grid_size)
         clahe_image = clahe.apply(cv_image)
 
         try:
@@ -29,6 +54,11 @@ class ImageSubscriber:
             rospy.logerr(e)
 
 if __name__ == '__main__':
+    print("starting image subscriber")
+    print(f"listening to topic1: {image_topic}")
+    print(f"listening to topic2: {image_topic2}")
+
+
     rospy.init_node('image_subscriber', anonymous=True)
     image_subscriber = ImageSubscriber()
     try:
